606e06d84e51fac760743c6388980d9cc21c88b8
vrek <ray.koons@gmail.com>
2022-12-12 19:49:42 -0600

    added to calculate changes improved unit tests for is_prime 	modified:   __pycache__/mathhelper.cpython-311.pyc 	modified:   __pycache__/test_mathhelper.cpython-311.pyc 	modified:   mathhelper.py 	modified:   test_mathhelper.py

--- mathhelper.py
+++ mathhelper.py
@@ -2,13 +2,26 @@
 import math
 
 def is_prime(number):
+    """This tests if a number is prime
+    1st argument must be int
+    Output is boolean, True if number is prime False if it is not
+    """
+    if isinstance(number) is not int:
+        raise TypeError("The input to this method must be a int")
     if number > 1:
+        if number == 2:
+            return True
         for i in range(2, math.floor(math.sqrt(number)+1)):
-            if(number % i) == 0:
+            if (number % i) == 0:
                 return False
-        else:
             return True
     return False
 
-def calculate_change(cost, amount_paid):  
+
+def calculate_change(cost[,,,,,,,,,], money_paid[,,,,,,,,,]):
+    """Returns the amount of change from a purchase
+    Input is a list, Returns a list
+    list is in format[100, 50, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01]"""
+    amount_paid = money_paid[0]*100+money_paid[1]*50+money_paid[2]*20+money_paid[3]*10+money_paid[4]*5+money_paid[5]*1+money_paid[6]*0.25+money_paid[7]*0.10+money_paid[8]*0.05+money_paid[9]*0.01
     change = amount_paid - cost
+    return change
--- test_mathhelper.py
+++ test_mathhelper.py
@@ -10,7 +10,9 @@ class testMathHelper(unittest.TestCase):
         self.assertEqual(is_prime(0),False)
         self.assertEqual(is_prime(7),True)
         self.assertEqual(is_prime(2),True)
-
-
+        """Tests that incorrect input data types are correctly handled"""
+        self.assertRaises(TypeError, is_prime, True)
+        self.assertRaises(TypeError, is_prime, 'five')
+        self.assertRaises(TypeError, is_prime, 4+6j)
 if __name__=='__main__':
     unittest.main()
