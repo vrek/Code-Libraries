8e04d47e1d6c1bd612c853bd3339d3512631dbe5
vrek <ray.koons@gmail.com>
2022-12-13 20:43:09 -0600

    testing

--- mathhelper.py
+++ mathhelper.py
@@ -2,8 +2,7 @@
 import math
 
 def is_prime(number):
-    """This tests if a number is prime
-    1st argument must be int
+    """This tests if a number is prim 
     Output is boolean, True if number is prime False if it is not
     """
     if isinstance(number) is not int:
@@ -18,10 +17,11 @@ def is_prime(number):
     return False
 
 
-def calculate_change(cost[,,,,,,,,,], money_paid[,,,,,,,,,]):
+def calculate_change(cost, money_paid):
     """Returns the amount of change from a purchase
     Input is a list, Returns a list
     list is in format[100, 50, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01]"""
-    amount_paid = money_paid[0]*100+money_paid[1]*50+money_paid[2]*20+money_paid[3]*10+money_paid[4]*5+money_paid[5]*1+money_paid[6]*0.25+money_paid[7]*0.10+money_paid[8]*0.05+money_paid[9]*0.01
+    amount_paid = money_paid[0]*100+money_paid[1]*50+money_paid[2]*20+money_paid[3]*10+money_paid[4]*5+money_paid[5]*1\
+        +money_paid[6]*0.25+money_paid[7]*0.10+money_paid[8]*0.05+money_paid[9]*0.01
     change = amount_paid - cost
     return change
