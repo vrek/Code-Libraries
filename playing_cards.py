82fbccfb1d630410d08abe8eebe64139811859eb
vrek <ray.koons@gmail.com>
2023-02-17 12:52:24 -0600

    got program working

--- playing_cards.py
+++ playing_cards.py
@@ -13,10 +13,10 @@ class PlayingCard(object):
         if face_value in face_values:
             self.face_value = face_value
         else:
-            raise TypeError
+            raise TypeError    
     def show(self):
+        """function to show the value of a playing card"""
         print(f"{self.face_value} of {self.suit}")
-
 class Deck:
     """Class to hold and store a deck of cards"""
     suits = ('hearts', 'diamonds', 'spades', 'clubs')
@@ -25,19 +25,22 @@ class Deck:
         self.cards = []
         self.build()
     def build(self):
+        """function to build a deck"""
         for s in self.suits:
             for v in self.face_values:
                 self.cards.append(PlayingCard(s,v))
     def shuffle(self):
+        """function to shuffle a deck"""
         for i in range(len(self.cards) -1, 0, -1):
             r = random.randint(0,i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
     def drawCard(self):
+        """function to draw a card"""
         return self.cards.pop()
     def show(self):
+        """function to display a hand"""
         for c in self.cards:
             c.show()
-
 class Player:
     """Class to hold an instance of a player"""
     def __init__(self):
@@ -45,10 +48,13 @@ class Player:
         self.hand_value = 0
         self.wins = 0
     def draw(self, deck):
+        """Function to draw a card"""
         self.hand.append(deck.drawCard())
         return self
     def showHand(self):
+        """Function to show their hand"""
         for card in self.hand:
             card.show()
     def foldHand(self):
+        """Function to fold their hand"""
         self.hand.clear()
---
+++ test.py
@@ -0,0 +1,9 @@
+# Write your solution here
+#number = input("Please type in a number: ")
+#print(f"Integer part: {int(number)}")
+#print(f"Decimal part: {number - int(number)}")
+
+#number = input("Please type in a number: ")
+#print(f"Integer part: {int(number)}")
+
+print(f"Integer part: {int(0.1)}")
